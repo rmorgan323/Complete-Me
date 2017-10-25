cf5cb95a663785e0c4da87d87ecfaf529dfbeaa5
Rob Morgan <rmorgan323@gmail.com>
2017-10-25 14:52:26 -0600

    refactors suggest function and adds tests

--- tests/Trie-test.js
+++ tests/Trie-test.js
@@ -11,16 +11,10 @@ describe('Trie', () => {
 		var completion = new Trie();
 		expect(completion).to.be.an('object');
 	})
+})
 
-	// it('should be able to insert a new word into the Trie', () => {
-	// 	var completion = new Trie();
-	// 	completion.insert('pizza');
-	// 	completion.insert('piece');
-	// console.log(JSON.stringify(completion, null, 2))
-
-	// })
-
-	it('should increment the word count when a word is added', () => {
+describe('Insert', () => {
+	it('should increment the word count when a word is inserted', () => {
 		var completion = new Trie();
 		expect(completion.count).to.equal(0);
 		completion.insert('pizza');
@@ -29,42 +23,80 @@ describe('Trie', () => {
 		expect(completion.count).to.equal(2);
 	})
 
+	it('should not duplicate words in the Trie', () => {
+		var completion = new Trie();
+		expect(completion.count).to.equal(0);
+		completion.insert('pizza');
+		expect(completion.count).to.equal(1);
+		completion.insert('pizza');
+		expect(completion.count).to.equal(1);
+	})
+})
+
+describe('Populate', () => {
+	it('should contain 234,371 words after populating with the dictionary', () => {
+    var completion = new Trie();
+  	completion.populate(dictionary);
+  	expect(completion.count).to.equal(234371);
+  });
+})
+
+describe('Select', () => {
+  it('should increment word influence every time a word is selected', () => {
+  	var completion = new Trie();
+  	completion.insert('pizza');
+  	expect(completion.root.children.p.children.i.children.z.children.z.children.a.popularity).to.equal(0)
+  	completion.select('pizza');
+  	expect(completion.root.children.p.children.i.children.z.children.z.children.a.popularity).to.equal(1)
+  	completion.select('pizza');
+  	expect(completion.root.children.p.children.i.children.z.children.z.children.a.popularity).to.equal(2)
+  })
+})
+
+describe('Suggest', () => {
 	it('should suggest completed words when given letters', () => {
 		var completion = new Trie();
 		completion.insert('pizza');
 		completion.insert('pizzazz');
-		// console.log(JSON.stringify(completion, null, 2))
+		completion.insert('apple')
 		expect(completion.suggest('piz')).to.deep.equal(['pizza', 'pizzazz']);
+		expect(completion.suggest('ap')).to.deep.equal(['apple']);
 	})
-
-	it('should contain 235,886 words after populating with the dictionary', () => {
-    var completion = new Trie();
-  	completion.populate(dictionary);
-  	expect(completion.count).to.equal(235886);
-  });
 
 	it('should return an empty array if there are no suggestions', () => {
     var completion = new Trie();
   	completion.populate(dictionary);
   	expect(completion.suggest('childhood')).to.deep.equal([]);
-  	// console.log(completion.suggest('childhood'));
-		// expect(completion.suggest('piz')).to.deep.equal(['pizza', 'pizzazz']);
+  	expect(completion.suggest('cy5dpzx2d8rwjv')).to.deep.equal([]);
+
   });
 
-  it('should return a large array of suggestions if common letters are entered', () => {
+	it('should suggest without case-sensitivity', () => {
+		var completion = new Trie();
+		completion.insert('pizza');
+		completion.insert('pIzZaZz');
+		completion.insert('PIZZERIA');
+		expect(completion.suggest('piz')).to.deep.equal(['pizza', 'pizzazz', 'pizzeria']);
+		expect(completion.suggest('PIZ')).to.deep.equal(['pizza', 'pizzazz', 'pizzeria']);
+	})
+
+  it('should return a large array of words when only a few common letters are entered', () => {
   	var completion = new Trie();
   	completion.populate(dictionary);
-  	expect(completion.suggest('ch').length).to.equal(2527);
+  	expect(completion.suggest('sen')).to.include.members(['sender', 'senate', 'sense', 'senile']);
   })
 
   it('should prioritize suggestions based on previous selections', () => {
   	var completion = new Trie();
   	completion.insert('pizza');
-  	completion.insert('pizzeria');
   	completion.insert('pizzazz');
+  	completion.insert('pizzeria');
 		expect(completion.suggest('piz')).to.deep.equal(['pizza', 'pizzazz', 'pizzeria']);
 		completion.select('pizzeria');
-		expect(completion.suggest('piz')).to.deep.equal(['pizzeria', 'pizza', 'pizzazz']);
+		expect(completion.suggest('piz')).to.deep.equal(['pizzeria', 'pizza', 'pizzazz' ]);
+		completion.select('pizza');
+		completion.select('pizza');
+		expect(completion.suggest('piz')).to.deep.equal(['pizza', 'pizzeria', 'pizzazz' ]);
   })
 
 })
--- tests/index-test.js
+++ tests/index-test.js
@@ -2,7 +2,7 @@ import {expect} from 'chai';
 import index from '../lib/index';
 import {bubbleSort, insertionSort, mergeSort, quickSort} from '@rmorgan323/sorting-suite';
 
-describe('Example Test File', () => {
+describe('Sort imports', () => {
 	it('should successfully use sorts we imported', () => {
 		expect(bubbleSort([3,2,1])).to.deep.equal([1,2,3]);
 	})
